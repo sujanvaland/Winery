d4b7cb93c79d8db9f0c6604b58ba782734ec8d8e
ajaybhavsar <abbhavsar4u@gmail.com>
2020-12-27 17:23:29 +0530

    commit ui

--- app/components/OverlayActivityIndicator.js
+++ app/components/OverlayActivityIndicator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { View, ActivityIndicator } from "react-native";
 import Styles from "../config/styles";
-const { color} = Styles;
+const { color } = Styles;
 // import { color } from "../config/styles";
 
 const OverlayActivityIndicatorElement = props => {
@@ -15,14 +15,14 @@ const OverlayActivityIndicatorElement = props => {
     alignItems: 'center',
     justifyContent: 'center',
     //backgroundColor: color.COLOR_BLACK,
-    height:"100%",
-    width:"100%",
-    opacity:0.6,
-    zIndex:9999999999,
+    height: "100%",
+    width: "100%",
+    opacity: 0.6,
+    zIndex: 9999999999,
   }
   return (
     <View pointerEvents="none" style={loadingStyle}>
-      <ActivityIndicator size='large' color="#444"/>
+      <ActivityIndicator size='large' color="#fff" />
     </View>
   )
 };
